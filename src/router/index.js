947f65adf92ea8aba237e9a4a11ac369c87dd5e3
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-02-09 12:32:54 +0300

    added all views for app

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +7,8 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    meta: { layout: 'main' },
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/login',
@@ -17,10 +17,34 @@ const routes = [
     component: () => import('../views/Login.vue')
   },
   {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Register.vue')
+  },
+  {
     path: '/categories',
     name: 'categories',
     meta: { layout: 'main' },
     component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main' },
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main' },
+    component: () => import('../views/Record.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main' },
+    component: () => import('../views/Profile.vue')
   }
 ]
 
