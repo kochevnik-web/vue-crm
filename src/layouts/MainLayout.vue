a660707c35ded76fe511883161bb1b5b7c371cca
Kovalev Dmitriy <kacevnik@yandex.ru>
2021-02-09 16:30:28 +0300

    created show/hide sidebar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="app-main-layout">
 
-    <Navbar />
+    <Navbar @click="isOpen = !isOpen"/>
 
-    <Sidebar />
+    <Sidebar v-model="isOpen" />
 
-    <main class="app-content">
+    <main class="app-content" :class="{full: !isOpen}">
       <div class="app-page">
           <router-view />
       </div>
@@ -24,6 +24,9 @@ import Navbar from '@/components/app/Navbar'
 import Sidebar from '@/components/app/Sidebar'
 export default {
   name: 'main-layout',
+  data: () => ({
+    isOpen: true
+  }),
   components: {
     Navbar, Sidebar
   }
